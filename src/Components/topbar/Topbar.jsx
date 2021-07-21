28e0ac666328bf78e16c6cb11b2c7e56e6fb7bc5
Kingsley Atuba <atubak40@gmail.com>
2021-07-21 19:50:48 +0100

    edit

--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className='topbar'>
       <div className='topbarWrapper'>
         <div className='topLeft'>
-          <span className='logo'>domadmin</span>
+          <span className='logo'>tubadmin</span>
         </div>
         <div className='topRight'>
           <div className='topbarIconContainer'>
