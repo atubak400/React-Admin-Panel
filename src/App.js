6e32d8609cb14fd40ad8eb7f22450a6b1eebef45
Kingsley Atuba <atubak40@gmail.com>
2021-07-22 22:57:59 +0100

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Topbar from './Components/topbar/Topbar'
+import Sidebar from './Components/sidebar/Sidebar'
+import './app.css'
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className='container'>
+        <Sidebar />
+        <div className='others'>other pages</div>
+      </div>
     </div>
   )
 }
