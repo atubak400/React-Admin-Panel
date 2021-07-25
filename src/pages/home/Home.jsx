74dbefa2645a963d673c7a930207e541ec14f237
Kingsley Atuba <atubak40@gmail.com>
2021-07-25 23:01:24 +0100

    chart component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import FeaturedInfo from '../../Components/featuredInfo/FeaturedInfo'
+
 import './home.css'
+import Chart from '../../Components/chart/Chart'
 
 export default function Home() {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart />
     </div>
   )
 }
