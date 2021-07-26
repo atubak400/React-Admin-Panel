403f23f07780222d8269000a426fe1af842b7f00
Kingsley Atuba <atubak40@gmail.com>
2021-07-26 12:49:04 +0100

    featured info chart done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
 import FeaturedInfo from '../../Components/featuredInfo/FeaturedInfo'
-
 import './home.css'
 import Chart from '../../Components/chart/Chart'
+import { userData } from '../../dummyData'
 
 export default function Home() {
   return (
     <div className='home'>
       <FeaturedInfo />
-      <Chart />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        grid
+        dataKey='Active User'
+      />
     </div>
   )
 }
