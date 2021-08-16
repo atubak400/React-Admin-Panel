7d09d72546d34550317a3f2f60142f9731a9a4e2
Kingsley Atuba <atubak40@gmail.com>
2021-08-16 10:34:40 +0100

    user information Section Done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import FeaturedInfo from '../../Components/featuredInfo/FeaturedInfo'
 import './home.css'
 import Chart from '../../Components/chart/Chart'
 import { userData } from '../../dummyData'
+import WidgetSm from '../../Components/WidgetSm/WidgetSm'
+import WidgetLg from '../../Components/WidgetLg/WidgetLg'
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
         grid
         dataKey='Active User'
       />
+      <div className='homeWidgets'>
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   )
 }
